7254e5d0e08d1862dba14e853d922f91ec0e23d5
Mark Pittaway <mark.pittaway@gmail.com>
2020-11-05 10:26:43 +1100

    chore(ci): Setup github actions for testing

---
+++ scripts/tests_setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Install python package dependencies
+sudo apt-get -y update
+sudo apt-get -y install libxml2-dev libxmlsec1-dev libxmlsec1-openssl
+
+# install libmagic
+wget http://launchpadlibrarian.net/433926958/libmagic-mgc_5.37-3_amd64.deb
+wget http://launchpadlibrarian.net/433926961/libmagic1_5.37-3_amd64.deb
+sudo dpkg -i libmagic-mgc_5.37-3_amd64.deb
+sudo dpkg -i libmagic1_5.37-3_amd64.deb
+
+# Update python core packages
+python -m pip install --upgrade pip wheel setuptools
+
+if [ "$SERVER" == "true" ]; then
+    cd src/server
+    pip install -r dev-requirements.txt
+    pip install -e .
+    cd ../../
+fi
+
+if [ "$SERVICES" == "true"]; then
+    docker-compose -f .travis-docker-compose.yml up -d
+    cd src/server
+    pip install -r requirements.txt
+    cd ../../
+    bash scripts/wait-elastic-online.sh
+fi
+
+if [ "$CLIENT" == "true" ]; then
+    cd src/clients/python
+    pip install -r dev-requirements.txt
+    pip install -e .
+    cd ../../../
+fi
+
+if [ "$START_SAMS" == "true" ]; then
+    cd src/server
+    (nohup honcho start &)
+    cd ../../
+fi
+
+if [ "$BEHAVE" == "true" ]; then
+    pip install -r tests/features/requirements.txt
+fi
